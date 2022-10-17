395c48f9c4757b0b03c5d178cb3868f4c761d8c2
SaajidNawwar <it19237114@my.sliit.lk>
2022-10-17 20:56:38 +0530

    updated ui

--- app/src/main/java/com/example/fuel/MainActivity.java
+++ app/src/main/java/com/example/fuel/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.fuel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.station_register);
+//        setContentView(R.layout.activity_main);
+//
+//
+//        btnLogin = (Button) findViewById(R.id.register);
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                register();
+//            }
+//        });
+//
+//        button = (Button) findViewById(R.id.login);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                login();
+//            }
+//        });
+//    }
+//
+//    public void register(){
+//        Intent intent = new Intent(this,Register.class);
+//        startActivity(intent);
+//    }
+//
+//    public void login(){
+//        Intent intent = new Intent(this,Login.class);
+//        startActivity(intent);
     }
 }
