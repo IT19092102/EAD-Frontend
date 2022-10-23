84bd074af7b1cc246ff9eb4fdd7fac821adfb749
SaajidNawwar <it19237114@my.sliit.lk>
2022-10-23 17:30:27 +0530

    spinner added, Ui theme changed, logo changed

--- app/src/main/java/com/example/fuel/StationRegister.java
+++ app/src/main/java/com/example/fuel/StationRegister.java
@@ -12,14 +12,32 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class StationRegister extends AppCompatActivity{
+public class StationRegister extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     TextView textView;
+    String[] locations = { "Select Station Location", "Ampara","Anuradhapura","Badulla","Batticaloa","Colombo","Galle", "Gampaha","Hambanthota","Jaffna","Kaluthara"};
+    String[] brands = {"Choose Fuel Brand", "CEYPETCO", "IOC"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.station_register);
+
+        //locations spinner
+        Spinner spinner = findViewById(R.id.stationLoc);
+        spinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter ad= new ArrayAdapter(this,android.R.layout.simple_spinner_item,locations);
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(ad);
+
+        //brands spinner
+        Spinner brandspinner = findViewById(R.id.stationBrand);
+        brandspinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter brandad=new ArrayAdapter(this, android.R.layout.simple_spinner_item,brands);
+        brandspinner.setAdapter(brandad);
+
 
         textView=(TextView) findViewById(R.id.stationSignInText);
         textView.setOnClickListener(new View.OnClickListener() {
@@ -31,10 +49,21 @@ public class StationRegister extends AppCompatActivity{
         });
     }
 
+
+
     public void navigateToStationLogin(){
         Intent intent = new Intent(this,StationLogin.class);
         startActivity(intent);
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
